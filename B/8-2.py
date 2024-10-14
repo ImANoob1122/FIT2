d1854475579f3440757b09e73d0fc7795d91142c
Ryosuke Shimizu <ymrs1122@gmail.com>
2024-10-14 15:53:55 +0900

    end 8-2

---
+++ B/8-2.py
@@ -0,0 +1,32 @@
+import pyxel
+CANVAS_SIZE = 200
+CANVAS_COLOR = 7
+CIRCLE_RADIUS = 10
+CIRCLE_COLOR = 6
+
+class Draw:
+    def __init__(self):
+        pyxel.init(CANVAS_SIZE, CANVAS_SIZE, title="event loop")
+        pyxel.cls(CANVAS_COLOR)
+        self.circleX, self.circleY = 100, 0
+        self.speed = 1
+        self.vx, self.vy = 0.866, 0.5 #cos and sin of 30dgree
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        self.circleX += self.vx * self.speed
+        self.circleY += self.vy * self.speed
+        if self.circleX < 0 or self.circleX > CANVAS_SIZE:
+            self.vx = self.vx * -1
+        if self.circleY > CANVAS_SIZE:
+            self.circleX, self.circleY = 100, 0
+            self.speed += 0.1
+
+    def draw(self):
+        pyxel.cls(CANVAS_COLOR)
+        pyxel.circ(self.circleX, self.circleY, CIRCLE_RADIUS, CIRCLE_COLOR)
+
+
+
+
+
+Draw()
