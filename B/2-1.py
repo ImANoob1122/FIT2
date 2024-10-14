de4aa8e3824a5002b8f0f2eb29c89c8cfdedc0ec
Ryosuke Shimizu <ymrs1122@gmail.com>
2024-10-14 13:06:26 +0900

    changeB2-1

--- B/2-1.py
+++ B/2-1.py
@@ -1,18 +1,16 @@
 import pyxel
 CANVAS_SIZE = 200
 CANVAS_COLOR = 7
-LINE_COUNT = 11
 LINE_INTERVAL = 10
 
 class Draw:
     def __init__(self):
-        pyxel.init(CANVAS_SIZE, CANVAS_SIZE)
+        pyxel.init(CANVAS_SIZE, CANVAS_SIZE, title="manyLine")
         pyxel.cls(CANVAS_COLOR)
-        for i in range(LINE_COUNT):
-            start_x = i*LINE_INTERVAL
-            end_x = i*LINE_INTERVAL + CANVAS_SIZE//2
-            print(start_x, end_x)
-            pyxel.line(start_x, 0, end_x, CANVAS_SIZE, 0)
+        end_x_start = CANVAS_SIZE//2
+        for i in range(0, end_x_start+1, LINE_INTERVAL):
+            end_x = i+end_x_start
+            pyxel.line(i, 0, end_x, CANVAS_SIZE, 0)
         pyxel.show()
 
 Draw()
